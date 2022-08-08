74f6ea6622d9964d29c3aad3adda6503fa2d7d92
Ahmed Hussein <ahmedhussein@Ahmeds-MacBook-Pro.local>
2022-08-08 12:50:18 -0600

    adding logic

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+
+const btn = document.getElementById('btn'); 
+
+const color = document.querySelector(".color"); 
+
+btn.addEventListener("click", function(){
+    //get random number between 0-3 just to access my array 
+    const randomNumber = getRandomColor(); 
+    console.log(randomNumber); 
+    document.body.style.backgroundColor = colors[randomNumber];
+    color.textContent = colors[randomNumber];
+})
+
+function getRandomColor(){
+    return Math.random()
+}
